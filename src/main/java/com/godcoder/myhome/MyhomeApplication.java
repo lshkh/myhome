9b47bdac099a7b2fbb36fe451fb71cf82c650122
leesh <lshkh123@daum.net>
2023-08-07 15:40:54 +0900

    init 2

--- src/main/java/com/godcoder/myhome/MyhomeApplication.java
+++ src/main/java/com/godcoder/myhome/MyhomeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyhomeApplication {
 
 	public static void main(String[] args) {
+		// main
 		SpringApplication.run(MyhomeApplication.class, args);
 	}
 
